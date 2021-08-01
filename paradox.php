26e93fa929d5beba27a641b787482511ac832f1b
GraysS <antonpichka@gmail.com>
2021-08-01 03:31:25 +0300

    update file paradox

--- paradox.php
+++ paradox.php
@@ -1,115 +1,47 @@
 <?php
 
-//namespace ZOV;
+namespace ZOV;
 
-require_once "DB/cParadox.php";
 require_once "MyFirebase.php";
-require_once "settings/settings.php";
 
 
-// CHARACTERS DB (TABLE,COLUMN).
-$nameTableDB = NAME_TABLE_DB;
-$ppk_id = PPK_ID_COLUMN_DB;
-$address = ADDRESS_COLUMN_DB;
-$isOn = IS_ON_COLUMN_DB;
-$number_object = NUMBER_OBJECT_COLUMN_DB;
-$latitudes = LATITUDE_COLUMN_DB;
-$longitudes = LONGITUDE_COLUMN_DB;
+$getData = json_decode(file_get_contents("php://input"));
 
-// УНИКАЛЬНЫЙ ПРЕФИКС ЕЛЕМЕНТА СООТВЕТСТВИЮ PPK/ПУЛЬТУ
-$type_pult = TYPE_PULT_FIREBASE;
+//print_r($getData->data->array_ppk);
+//echo '<pre>'.print_r(json_decode(file_get_contents("php://input")),1).'</pre>';
 
-// CHARACTERS DB REQUEST (COLUMN) Example: SELECT * FROM $nameTableDB WHERE requestIsOn = true;
-$requestColumnIsOn = IS_EXISTS_IS_ON_COLUMN_DB; // Значение должно совпадать с isOn если есть такой столбец
+$dataMassive = $getData->data->array_ppk;
+$ppk_id = $getData->data->ppk_id;
+$address = $getData->data->address;
+$isOn = $getData->data->isOn;
+$number_object = $getData->data->number_object;
+$latitude = $getData->data->latitude;
+$longitude =$getData->data->longitude;
+$type_pult = $getData->data->type_pult;
 
-$values = $_POST["values"];
 // Установка timezone Киев как основа
 date_default_timezone_set('Europe/Kiev');
 
 $path = "google-service-account.json";
 
-$pdx = new cParadox();
-$pdx->m_default_field_value = "?";//" ";
+$myFirebase = new MyFirebase($path); 
 
-if ($pdx->Open($nameTableDB)) {
-     $tot_rec = $pdx->GetNumRecords();
-    if ($tot_rec) {
+$iterationResultFalier = 0;
+$lenghtGetListPPK = 0;
 
-        $myFirebase = new MyFirebase($path); 
+foreach($dataMassive as $item) {
+    $lenghtGetListPPK++;
+           
+    $ppk_id_column = $item->$ppk_id;
+    $address_column = $item->$address;
+    $isOn_column = $item->$isOn;
+    $number_object_column = $item->$number_object;
+    $latitude_column = $item->$latitude;
+    $longitude_column = $item->$longitude;
 
-        $iterationResultFalier = 0;
-        $lenghtGetListPPK = 0;
-        for($rec=0; $rec<$tot_rec; $rec++) {
-            $lenghtGetListPPK++;
-           /*
-            print_r($item);
-            echo "<tr>";
-            echo "<td>" . $rec;
-            echo "<td>" . $item['EVENTTIME'];
-            echo "<td>" . $item['ACCOUNT'];
-            echo "<td>" . $item['EVENT'];
-            */
-            $item = $pdx->GetRecord($rec);
+    $timestamp = time() + 60*3; // now + 3 minutes
 
-            $ppk_id_column = $item[$ppk_id];
-            $address_column = $item[$address];
-            $isOn_column = $item[$isOn];
-            $number_object_column = $item[$number_object];
-            $latitude_column = $item[$latitudes];
-            $longitude_column = $item[$longitudes];
-
-            if(!isset($ppk_id_column) || !isset($address_column)) {
-                continue;
-            }
-
-            if(!isset($number_object_column)) {
-                $number_object_column = "unknown";
-            }
-
-            if(!isset($isOn_column)) {
-                $isOn_column = true;
-            }
-
-            if(!isset($latitude_column)) {
-                $latitude_column = 0.0;
-            }
-
-            if(!isset($longitude_column)) {
-                $longitude_column = 0.0;
-            }
-
-            if($requestColumnIsOn == $isOn) {
-                if(is_numeric($isOn_column)) {
-                    if($isOn_column == 1 || $isOn_column == 1.0) {
-                        $isOn_column = true;
-                    } else {
-                        $isOn_column = false;
-                    }
-                } elseif(is_bool($isOn_column) === true) {
-                    if($isOn_column) 
-                        $isOn_column = true;
-                    else 
-                        $isOn_column = false;
-                } elseif (is_string($isOn_column)) {
-                    if($isOn_column == "1" || $isOn_column == "1.0" || $isOn_column == "true") {
-                        $isOn_column = true;
-                    } else {
-                        $isOn_column = false;
-                    }
-                } else {
-                    $isOn_column = true;
-                }
-            } else {
-                $isOn_column = true;
-            }
-
-            if(!$isOn_column) {
-                continue;
-            }
-
-            $timestamp = time() + 60*3; // now + 3 minutes
-
-            $result = $myFirebase->setDocument(
+    $result = $myFirebase->setDocument(
                                 $ppk_id_column,
                                 $address_column,
                                 $isOn_column,
@@ -120,25 +52,16 @@ if ($pdx->Open($nameTableDB)) {
                                 $timestamp
                             );
 
-            if(!$result) {
-                $iterationResultFalier++;   
-            }
-
-        } // end for
-
-        if($iterationResultFalier == $lenghtGetListPPK) {
-            $response["http_code"] = "500";
-            echo json_encode($response);
-        } else {
-            $response["http_code"] = "200";
-            echo json_encode($response);
-        }
-    } else {
-        $response["message"] = "Empty Table";
-        echo json_encode($response);
+    if(!$result) {
+        $iterationResultFalier++;   
     }
-    $pdx->Close(); 
+
+} // end for
+
+if($iterationResultFalier == $lenghtGetListPPK) {
+    $response["http_code"] = "500";
+    echo json_encode($response);
 } else {
-	$response["http_code"] = "500";
-	echo json_encode($response);
+    $response["http_code"] = "200";
+    echo json_encode($response);
 }
